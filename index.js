b2014459a846df958a130d3484775883cfb56db0
shabnam <shabnam.mostary@gmail.com>
2020-10-29 22:31:09 -0400

    added validation for gameId check

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
+
+
+function gameIdcounter(arr) {
+  let gameIdArr = arr.map((player) => {
+    return player.gameId
+  })
+  let gameIdCount = gameIdArr.reduce((acc, element) => {
+    if (element in acc) {
+      acc[element]++
+    } else {
+      acc[element] = 1
+    }
+
+    return acc
+  }, {})
+
+  return Math.max(...Object.values(gameIdCount)) > 3
+}
+// console.log(gameIdcounter(lineup))
+
+
 function validateLineup(arr) {
+  let lineupLengthCheck = (arr.length !== 9)
+  let salaryCheck = true
+  let gameIdCheck = gameIdcounter(arr)
+  // let teamIdCheck = true
+  // let positionCheck = true
+
   let salary = arr.reduce((acc, element) => {
     return acc += element.salary
   }, 0)
 
-  if (salary > 45000 || arr.length > 9) {
-    return false
-  } else {
-    return true
+  if (salary > 45000) {
+    salaryCheck = false
   }
+
+  return (lineupLengthCheck && salaryCheck && gameIdCheck)
 }
 
 module.exports = validateLineup
