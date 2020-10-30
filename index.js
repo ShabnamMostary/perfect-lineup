d05015e9c3ac826f5895c427724a1b8f27d4d1b6
shabnam <shabnam.mostary@gmail.com>
2020-10-30 00:17:55 -0400

    made a helper function to remove repeated code

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ function teamIdCounter(arr) {
   let teamIdArr = arr.map((element) => {
     return element.teamId
   })
-  let teamIdCount = (teamIdArr).reduce((acc, element) => {
+
+  return maxCounter(teamIdArr) > 2
+}
+function maxCounter(arr) {
+  let count = (arr).reduce((acc, element) => {
     if (element in acc) {
       acc[element]++
     } else {
@@ -12,27 +16,16 @@ function teamIdCounter(arr) {
     return acc
   }, {})
 
-  return Math.max(...Object.values(teamIdCount)) > 2
+  return Math.max(...Object.values(count))
 }
 
 function gameIdCounter(arr) {
   let gameIdArr = arr.map((player) => {
     return player.gameId
   })
-  let gameIdCount = gameIdArr.reduce((acc, element) => {
-    if (element in acc) {
-      acc[element]++
-    } else {
-      acc[element] = 1
-    }
 
-    return acc
-  }, {})
-
-  return Math.max(...Object.values(gameIdCount)) > 3
+  return maxCounter(gameIdArr) > 3
 }
-
-
 
 function validateLineup(arr) {
   let lineupLengthCheck = (arr.length !== 9)
@@ -47,7 +40,7 @@ function validateLineup(arr) {
     salaryCheck = false
   }
 
-  return (lineupLengthCheck && salaryCheck && gameIdCheck)
+  return (lineupLengthCheck && salaryCheck && gameIdCheck && teamIdCheck)
 }
 
 module.exports = validateLineup
