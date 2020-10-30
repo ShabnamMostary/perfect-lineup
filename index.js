f92189dea1e0720e82e304910ed18efae927b212
shabnam <shabnam.mostary@gmail.com>
2020-10-29 23:58:12 -0400

    added validation for teamId check

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+function teamIdCounter(arr) {
+  let teamIdArr = arr.map((element) => {
+    return element.teamId
+  })
+  let teamIdCount = (teamIdArr).reduce((acc, element) => {
+    if (element in acc) {
+      acc[element]++
+    } else {
+      acc[element] = 1
+    }
 
+    return acc
+  }, {})
 
-function gameIdcounter(arr) {
+  return Math.max(...Object.values(teamIdCount)) > 2
+}
+
+function gameIdCounter(arr) {
   let gameIdArr = arr.map((player) => {
     return player.gameId
   })
@@ -16,16 +31,14 @@ function gameIdcounter(arr) {
 
   return Math.max(...Object.values(gameIdCount)) > 3
 }
-// console.log(gameIdcounter(lineup))
+
 
 
 function validateLineup(arr) {
   let lineupLengthCheck = (arr.length !== 9)
   let salaryCheck = true
-  let gameIdCheck = gameIdcounter(arr)
-  // let teamIdCheck = true
-  // let positionCheck = true
-
+  let gameIdCheck = gameIdCounter(arr)
+  let teamIdCheck = teamIdCounter(arr)
   let salary = arr.reduce((acc, element) => {
     return acc += element.salary
   }, 0)
