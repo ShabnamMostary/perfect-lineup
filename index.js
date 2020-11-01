99ff1e0d8e324c6fd7c0fd2ebfa13041906f9b6f
shabnam <shabnam.mostary@gmail.com>
2020-11-01 00:43:51 -0400

    updated the helper function to compute frequency of any property

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-function teamIdCounter(arr) {
-  let teamIdArr = arr.map((element) => {
-    return element.teamId
+/* this function takes an array of objects & array property name and returns frequeny of different
+ values of that property */
+function frequencyCounter(arr, property) {
+  let frequencyArr = arr.map((element) => {
+    return element[property]
   })
 
-  return Math.max(...(Object.values(frequencyCounter(teamIdArr)))) <= 2
-}
-
-function frequencyCounter(arr) {
-  let count = (arr).reduce((acc, element) => {
+  let count = (frequencyArr).reduce((acc, element) => {
     if (element in acc) {
       acc[element]++
     } else {
@@ -19,20 +17,9 @@ function frequencyCounter(arr) {
 
   return count
 }
-
-function gameIdCounter(arr) {
-  let gameIdArr = arr.map((player) => {
-    return player.gameId
-  })
-
-  return Math.max(...(Object.values(frequencyCounter(gameIdArr)))) <= 3
-}
+// this function checks if the lineup have the right number of players at each position
 function positionChecker(arr) {
-  let positionArr = arr.map((element) => {
-    return element.position
-  })
-
-  let positionCounter = frequencyCounter(positionArr)
+  let positionCounter = frequencyCounter(arr, 'position')
 
   if (positionCounter.P === 1 && positionCounter.C === 1 && positionCounter['1B'] === 1 &&
     positionCounter['2B'] === 1 && positionCounter['3B'] === 1 && positionCounter.SS === 1) {
@@ -44,17 +31,15 @@ function positionChecker(arr) {
 
 function validateLineup(arr) {
   let lineupLengthCheck = (arr.length === 9)
-  let salaryCheck = true
-  let gameIdCheck = gameIdCounter(arr)
-  let teamIdCheck = teamIdCounter(arr)
+  // take the object returned by frequencyCounter and compute the maximum frequency
+  let gameIdCheck = Math.max(...(Object.values(frequencyCounter(arr, 'gameId')))) <= 3
+  let teamIdCheck = Math.max(...(Object.values(frequencyCounter(arr, 'teamId')))) <= 2
   let positionCheck = positionChecker(arr)
   let salary = arr.reduce((acc, element) => {
     return acc += element.salary
   }, 0)
 
-  if (salary > 45000) {
-    salaryCheck = false
-  }
+  let salaryCheck = salary <= 45000
 
   return (lineupLengthCheck && salaryCheck && gameIdCheck && teamIdCheck && positionCheck)
 }
